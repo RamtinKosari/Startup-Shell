0e664d61331e261a02a4670495304f178f95fb81
RamtinKosari <ramtinkosari@gmail.com>
2023-08-04 13:12:44 +0330

    Add Configuration and Shell Files

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+open_terminals() {
+    # - Open Browser Terminal
+    echo hi
+    gnome-terminal
+    gnome-terminal
+    gnome-terminal
+    gnome-terminal
+    gnome-terminal
+    gnome-terminal
+    gnome-terminal -- "brave-browser"
+    # - Open Project #1 Terminal
+    # - Open Project #2 Terminal
+    # - Open Project #3 Terminal
+}
+open_terminals
